059e11ae0904820bf5f51e8ed435a0e726d18b83
SanketShirke <95741537+SanketShirke@users.noreply.github.com>
2024-06-08 22:06:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,37 +2,41 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_key_pair" "sanketkey" {
-    key_name = "sanket-terrakey"
-    public_key = file("/home/ubuntu/.ssh/sanket-terrakey.pub")
+resource "aws_key_pair" "devops-decoded-my-keys" {
+    key_name = "devops-decoded-key"
+    public_key = file("/home/ubuntu/.ssh/devops-decoded-key.pub")
+    
+} 
+
+resource "aws_default_vpc" "devops-decoded-default-vpc" {
+
 }
 
-resource "aws_default_vpc" "default_vpc1" {
+resource "aws_security_group" "devops-decoded-allow-ssh" {
+    name = "devops-decoded-allow-ssh"
+    description = "Allow ssh from inbound traffic"
+    vpc_id = aws_default_vpc.devops-decoded-default-vpc.id
 
-}
-
-resource "aws_security_group" "sanket-ssh" {
-    name = "sanket-ssh"
-    description = "Allow ssh inbound traffic"
-    vpc_id = aws_default_vpc.default_vpc1.id
-    ingress{
+    ingress {
         description = "TLS from VPC"
         protocol = "TCP"
         from_port = 22
         to_port = 22
-        cidr_blocks= ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+
     }
     tags = {
-        Name = "sanket-ssh"
+        Name = "devops-decoded-allow-ssh"
     }
 }
 
-resource "aws_instance" "sanket-instance" {
-    key_name = aws_key_pair.sanketkey.key_name
-    ami = var.ec2-ubuntu-ami
+resource "aws_instance" "devops-decoded-instance" {
+    key_name = aws_key_pair.devops-decoded-my-keys.key_name
+    ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.sanket-ssh.name]
+    security_groups = [aws_security_group.devops-decoded-allow-ssh.name]
     tags = {
-        Name = "terra-auto-instance"
+        Name = "Secured Instance"
     }
+
 }
